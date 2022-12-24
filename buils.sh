40738dbfff856fcb8e66c73b3d03abe04621c4c3
Captain785 <thakurmukeshkumar785@gmail.com>
2022-12-24 22:40:29 +0530

    added local to remote git repository

---
+++ buils.sh
@@ -0,0 +1,10 @@
+echo " BUILD START"
+python3 -m pip install -r requirements.txt
+python3 manage.py collectstatic --noinput --clear
+
+echo " Making migrations "
+
+python3 manage.py makemigrations
+python3 manage.py migrate
+
+echo " BUILD END" 
