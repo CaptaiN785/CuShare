de9cc723af0d3f713c212ccab1a4b9378fe77d54
Captain785 <thakurmukeshkumar785@gmail.com>
2023-02-23 00:41:40 +0530

    updated table view and landing page

--- core/views.py
+++ core/views.py
@@ -2,4 +2,6 @@ from django.shortcuts import render, redirect
 
 # Create your views here.
 def home(request):
+    return redirect('server')
+    ## directly redirected to server page
     return render(request, 'core/index.html')
